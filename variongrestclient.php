8e4c9664e43aedd38e77377208c0a5de48eab008
Slavoj SANTA Hruška <santa@rsnet.sk@users.noreply.github.com>
2020-08-31 19:40:11 +0200

    Initial class commit

---
+++ variongrestclient.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * VarioNG VOLVO REST Client
+ * https://github.com/IntelliDust/VarioNG.git
+ * (c) 2020 Slavoj SANTA Hruska <vyvoj@santa3d.sk>
+ */
+
+class VarioNGClientException extends Exception {}
+
+class VarioNGClient implements Iterator, ArrayAccess {
+    
+    
+    public function __construct($options=[]){
+        $default_options = [
+            'user_agent' => "PHP VarioNGClient/0.1.7", 
+            'base_url' => NULL, 
+            'password' => NULL
+        ];
+        
+        $this->options = array_merge($default_options, $options);
+    }
+    
+}
+
+
